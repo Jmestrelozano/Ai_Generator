8805243703ee6bd0c12a0487f3528d4959c1b20d
Jmestrelozano <jmestrelozano@gmail.com>
2023-10-23 13:27:32 -0500

    update dependencies

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -20,7 +20,7 @@ export const LandingHero = () => {
                 "Chatbot.",
                 "Photo Generation.",
                 "Blog Writing.",
-                "Mail Writing."
+                "Mail Writing.",
               ],
               autoStart: true,
               loop: true,
@@ -33,7 +33,9 @@ export const LandingHero = () => {
       </div>
       <div>
         <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
-          <Button variant="premium" className="md:text-lg p-4 md:p-6 rounded-full font-semibold">
+          <Button
+            variant="premium"
+            className="md:text-lg p-4 md:p-6 rounded-full font-semibold">
             Start Generating For Free
           </Button>
         </Link>
